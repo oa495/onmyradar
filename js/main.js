ce748ff3d23049079677d8dbc3b735062f204844
Omayeli Arenyeka <oa495@nyu.edu>
2015-10-08 16:14:27 -0400

    storing weekly tasks in object

--- js/main.js
+++ js/main.js
@@ -1,13 +1,29 @@
-var tasksForWeek = {};
+var to_dos = {};
+var daysOfWeek = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+var weeklyTasks = [];
+var tasksForWeek;
 function runMainProgram() {	
-	console.log('here');
+	var tempArray = [];
+	tasksForWeek = to_dos['to-dos'];
+	for (var i = 0; i < daysOfWeek.length; i++) {
+		tempArray.push(tasksForWeek.filter(function(obj) {
+			if (obj.dayOfWeek === daysOfWeek[i]) {
+				return obj;
+			}
+		}));
+		weeklyTasks[i] = tempArray;
+		tempArray = [];
+	}
+	for (var i = 0; i < weeklyTasks.length; i++) {
+		console.log(JSON.stringify(weeklyTasks[i]));
+	}
 }
 
 $.ajax({
 	dataType:'json',
 	url:'to-dos.json',
 	success: function(returnData, statusValue, weirdNewObject) { 
-		tasksForWeek = returnData;
+		to_dos = returnData;
 		console.log ('got it, move along'); 
 		runMainProgram();
 	},
