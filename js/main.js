ae97ebc8f6b1b4c0c71ad2ec381f3e94231969f6
Omayeli Arenyeka <oa495@nyu.edu>
2015-10-09 00:36:41 -0400

    mapping task circles to priority and drawing on equivalent day rings

--- js/main.js
+++ js/main.js
@@ -1,15 +1,17 @@
 
 var to_dos = {};
-var areaOfScreenForDay = {}
+var areaOfScreenForDay = {};
 var weeklyTasks = {};
 var daysOfWeek = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
 var ready, paper;
-var taskCircles = [];
+var taskCircles = {};
 var dayCircles = [];
+var circlesForDay;
 
 function runMainProgram() {	
 	var tempArray = [];
 	tasksForWeek = to_dos['to-dos'];
+	var dayRadius = 400;
 	for (var i = 0; i < daysOfWeek.length; i++) {
 		tempArray = tasksForWeek.filter(function(obj) {
 			if (obj.dayOfWeek === daysOfWeek[i]) {
@@ -17,7 +19,8 @@ function runMainProgram() {
 			}
 		});
 		weeklyTasks[daysOfWeek[i]] = tempArray;
-		areaOfScreenForDay[daysOfWeek[i]] = Math.random() * Math.PI * 2;
+		dayRadius-=50;
+		areaOfScreenForDay[daysOfWeek[i]] = dayRadius;
 		tempArray = [];
 	}
 	//console.log(JSON.stringify(weeklyTasks));
@@ -29,13 +32,6 @@ function runMainProgram() {
 	} */
 	ready = true;
 }
-function getRandomPoint(radius, angle) {
-    return {
-        x: Math.cos(angle) * radius,
-        y: Math.sin(angle) * radius
-    };
-}
-
 $.ajax({
 	dataType:'json',
 	url:'to-dos.json',
@@ -51,7 +47,7 @@ $.ajax({
 }); 
 
 
-$( document ).ready(function() {
+$(document).ready(function() {
 	 $( "#start-visual" ).click(function() {
 	  startVisualization();
 	});
@@ -64,23 +60,42 @@ function startVisualization() {
 		console.log('visualization started!');
 		 paper = new Raphael(document.getElementById('visualization'), 1200, 850);
 		 // dow = day of week
-		 var dayRadius = 400;
 		 	var centerX = paper.canvas.offsetWidth/2+150;
 			var centerY = paper.canvas.offsetHeight/2; 
 		 for (var dow in weeklyTasks) {
 			  if (weeklyTasks.hasOwnProperty(dow)) {
-			  	 dayCircles.push(paper.circle(centerX, centerY, dayRadius-=50));
-		   		 drawTaskCircles(dow);
+			  	 dayCircles.push(paper.circle(centerX, centerY, areaOfScreenForDay[dow]));
+		   		 drawTaskCircles(dow, areaOfScreenForDay[dow], centerX, centerY);
 		  	}
 		}
 	}
 }
 
 
-function drawTaskCircles(dow) {
+function drawTaskCircles(dow, radius, centerX, centerY) {
 	var points;
-	var tasksForWeek = weeklyTasks[dow];
-	for (var i = 0; i < tasksForWeek.length; i++) {
-		var tasksForDay = tasksForWeek[i];
-	}
+	var priority;
+	var tempArray = [];
+	var tasksForDay = weeklyTasks[dow];
+	for (var i = 0; i < tasksForDay.length; i++) {
+		points = getRandomPoint(radius, centerX, centerY, i, tasksForDay.length);
+		priority = tasksForDay[i].priority;
+		var x = points.x;
+		var y = points.y;
+		console.log(x);
+		console.log(y);
+		paper.circle(x, y, priority*4).red(); 
+	} 
+	//taskCircles[dow] = tempArray;
+	//tempArray = [];
 }
+
+function getRandomPoint(radius, centerX, centerY, i, items) {
+    return {
+        x: centerX + radius * Math.cos(2 * Math.PI * i / items),
+        y: centerY + radius * Math.sin(2 * Math.PI * i / items) 
+    };
+}
+Raphael.el.red = function () {
+    this.attr({fill: "#f00"});
+};
