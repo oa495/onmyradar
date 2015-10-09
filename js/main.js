34fed01d17f2b1b3de440f89ad6f58cae5224a71
Omayeli Arenyeka <oa495@nyu.edu>
2015-10-08 22:33:04 -0400

    drawing circles to the screen with size determined by priority

--- js/main.js
+++ js/main.js
@@ -1,20 +1,28 @@
+
 var to_dos = {};
 var daysOfWeek = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
-var weeklyTasks = [];
+var weeklyTasks = {};
 var tasksForWeek;
+var ready;
+var allCircles = [];
+var paper;
 function runMainProgram() {	
+	var tempArray = [];
 	tasksForWeek = to_dos['to-dos'];
 	for (var i = 0; i < daysOfWeek.length; i++) {
-		weeklyTasks.push(tasksForWeek.filter(function(obj) {
+		tempArray = tasksForWeek.filter(function(obj) {
 			if (obj.dayOfWeek === daysOfWeek[i]) {
 				return obj;
 			}
-		}));
+		});
+		weeklyTasks[daysOfWeek[i]] = tempArray;
+		tempArray = [];
 	}
 	//console.log(JSON.stringify(weeklyTasks));
-	for (var i = 0; i < weeklyTasks.length; i++) {
-		console.log(JSON.stringify(weeklyTasks[i]));
-	}
+	/*for (var j = 0; j < daysOfWeek.length; j++) {
+		console.log(JSON.stringify(weeklyTasks[daysOfWeek[j]]));
+	}*/
+	ready = true;
 }
 
 $.ajax({
@@ -29,4 +37,43 @@ $.ajax({
 		 var err = eval("(" + xhr.responseText + ")");
   		 alert(err.Message);
 	}
+}); 
+
+
+$( document ).ready(function() {
+	 $( "#start-visual" ).click(function() {
+	  startVisualization();
+	});
 });
+
+/***** VISUALIZATIONNNNNNN ***/
+function startVisualization() {
+	console.log('here');
+	if (ready == true) {
+		console.log('visualization started!');
+		 paper = new Raphael(document.getElementById('visualization'), 1200, 850);
+		 // dow = day of week
+		 for (var dow in weeklyTasks) {
+			  if (weeklyTasks.hasOwnProperty(dow)) {
+		   		 drawTaskCircles(dow);
+		  	}
+		}
+	}
+}
+
+
+function drawTaskCircles(dow) {
+	var x, y = 200;
+	var tasksForWeek = weeklyTasks[dow];
+	for (var i = 0; i < tasksForWeek.length; i++) {
+		var tasksForDay = tasksForWeek[i];
+		for (var task in tasksForDay ) {
+			var priority = tasksForDay.priority*20;
+			x += Math.random() * (800 - 60) + 60;
+			y += Math.random() * (680 - 80) + 80;
+			allCircles.push(paper.circle(x, y, priority));
+			x = 200;
+			y = 200;
+		}
+	}
+}
