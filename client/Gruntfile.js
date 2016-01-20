022fb883ed2cd302e6206aa2edf331810deaa9e2
Omayeli Arenyeka <aomayeli@gmail.com>
2016-01-20 10:04:47 -0500

    using grunt with heroku

--- client/Gruntfile.js
+++ client/Gruntfile.js
@@ -488,7 +488,23 @@ module.exports = function (grunt) {
     'connect:test',
     'karma'
   ]);
-
+   grunt.registerTask('heroku', [
+    'clean:dist',
+    'wiredep',
+    'useminPrepare',
+    'concurrent:dist',
+    'postcss',
+    'ngtemplates',
+    'concat',
+    'ngAnnotate',
+    'copy:dist',
+    'cdnify',
+    'cssmin',
+    'uglify',
+    'filerev',
+    'usemin',
+    'htmlmin'
+  ]);
   grunt.registerTask('build', [
     'clean:dist',
     'wiredep',
