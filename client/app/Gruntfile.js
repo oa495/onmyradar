7a2c72dff810f5fe59c6178e9b1aea2392f57461
Omayeli Arenyeka <aomayeli@gmail.com>
2016-01-20 11:55:41 -0500

    moving dev dependencies

--- client/app/Gruntfile.js
+++ client/app/Gruntfile.js
@@ -488,23 +488,6 @@ module.exports = function (grunt) {
     'connect:test',
     'karma'
   ]);
-   grunt.registerTask('heroku', [
-    'clean:dist',
-    'wiredep',
-    'useminPrepare',
-    'concurrent:dist',
-    'postcss',
-    'ngtemplates',
-    'concat',
-    'ngAnnotate',
-    'copy:dist',
-    'cdnify',
-    'cssmin',
-    'uglify',
-    'filerev',
-    'usemin',
-    'htmlmin'
-  ]);
    
   grunt.registerTask('build', [
     'clean:dist',
@@ -520,8 +503,7 @@ module.exports = function (grunt) {
     'cssmin',
     'uglify',
     'filerev',
-    'usemin',
-    'htmlmin'
+    'usemin'
   ]);
 
   grunt.registerTask('default', [
